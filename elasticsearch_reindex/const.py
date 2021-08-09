10953904fbcbf093f2d514afff252fa8439d28da
BorysOl <oleynik.boris@gmail.com>
2021-08-09 14:30:45 +0300

    Add default Elasticsearch arguments

--- elasticsearch_reindex/const.py
+++ elasticsearch_reindex/const.py
@@ -14,12 +14,15 @@ HEADERS = {
     "Content-Type": "application/json",
 }
 
+# Default arguments for ElasticSearch client init.
+DEFAULT_ES_KWARGS = {"max_retries": 2, "timeout": 5, "retry_on_timeout": False}
+
 # Endpoint for create internal ElasticSearch reindex task.
 ES_CREATE_TASK_ENDPOINT = "{es_host}/_reindex?pretty&wait_for_completion=false"
 ES_CHECK_TASK_ENDPOINT = "{es_host}/_tasks/{task_id}"
 
 # Elasticsearch test index.
-ES_TEST_INDEX = "test_index"
+ES_TEST_INDEX = "test_index_{number}"
 
 # Logging message format.
 LOG_FORMAT = "[%(asctime)s] %(message)s"
@@ -29,6 +32,10 @@ DEFAULT_CONCURRENT_TASKS = 1
 
 # Variables for testing.
 TEST_ENV = os.getenv("ENV") == "test"
+
 ES_SOURCE_HOST = os.getenv("LOCAL_IP")
 ES_SOURCE_PORT = os.getenv("ES_SOURCE_PORT")
+ES_DEST_PORT = os.getenv("ES_DEST_PORT")
+
 LOCAL_SOURCE_HOST = f"http://{ES_SOURCE_HOST}:{ES_SOURCE_PORT}"
+LOCAL_DEST_HOST = f"http://{ES_SOURCE_HOST}:{ES_DEST_PORT}"
