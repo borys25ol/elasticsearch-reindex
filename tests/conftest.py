bd6212aeaec4fa4fe794e560b2414bdaeae5ce81
BorysOl <oleynik.boris@gmail.com>
2022-04-02 00:14:43 +0300

    Update project tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,37 @@
 from datetime import datetime
 from time import sleep
+from typing import Generator, List
 
 import pytest
 
-from elasticsearch_reindex import const
+from elasticsearch_reindex.client import ElasticsearchClient
 from elasticsearch_reindex.manager import Manager
 from elasticsearch_reindex.reindex import ReindexService
 from elasticsearch_reindex.utils import bulk_insert
+from tests.config import ES_TEST_INDEX, LOCAL_DEST_HOST, LOCAL_SOURCE_HOST, TEST_ENV
 
 TEST_INDEXES_COUNT = 3
 DOCS_COUNT = 10000
 
 DEFAULT_INPUT_PARAMS = {
-    "source_host": const.LOCAL_SOURCE_HOST,
-    "dest_host": const.LOCAL_DEST_HOST,
+    "source_host": LOCAL_SOURCE_HOST,
+    "dest_host": LOCAL_DEST_HOST,
     "check_interval": 10,
     "concurrent_tasks": 2,
 }
 
 
+@pytest.fixture(autouse=True)
+def check_testing_mode():
+    """
+    Check that testing env was set before each test.
+    """
+    assert TEST_ENV is True
+    yield
+
+
 @pytest.fixture()
-def test_messages():
+def test_messages() -> List[dict]:
     """
     Return messages with different id.
     """
@@ -36,47 +47,56 @@ def test_messages():
 
 
 @pytest.fixture()
-def reindex_service():
+def reindex_service() -> ReindexService:
     """
     Return initialized reindex service.
     """
     reindex_service = ReindexService(
-        source_es_host=const.LOCAL_SOURCE_HOST, dest_es_host=const.LOCAL_DEST_HOST
+        source_es_host=LOCAL_SOURCE_HOST, dest_es_host=LOCAL_DEST_HOST
     )
     return reindex_service
 
 
 @pytest.fixture()
-def insert_indexes(test_messages, reindex_service: ReindexService):
+def elastic_client() -> ElasticsearchClient:
+    """
+    Return initialized reindex service.
+    """
+    es_client = ElasticsearchClient(
+        source_es_host=LOCAL_SOURCE_HOST, dest_es_host=LOCAL_DEST_HOST
+    )
+    return es_client
+
+
+@pytest.fixture()
+def insert_indexes(
+    test_messages: List[dict], elastic_client: ElasticsearchClient
+) -> Generator[ReindexService, None, None]:
     """
     Return reindex service.
     """
     for index_number in range(TEST_INDEXES_COUNT):
-        es_index = const.ES_TEST_INDEX.format(number=index_number)
+        es_index = ES_TEST_INDEX.format(number=index_number)
         bulk_insert(
             docs=test_messages,
-            es_client=reindex_service.source_client,
+            es_client=elastic_client.source_client,
             es_index=es_index,
         )
-
     # Wait some time when data will reach Elasticsearch.
     sleep(2)
-
-    yield reindex_service
+    yield elastic_client
 
 
 @pytest.fixture()
-def manager(reindex_service: ReindexService):
+def manager(elastic_client: ElasticsearchClient) -> Generator[Manager, None, None]:
     """
     Return initialized manager class for start reindexing.
     """
     manager_class = Manager.from_dict(data=DEFAULT_INPUT_PARAMS)
-    manager_class.start_reindex()
-
-    yield
+    yield manager_class
 
     # Clear all of test indexes from source ans destination Elastics.
     for index_number in range(TEST_INDEXES_COUNT):
-        index = const.ES_TEST_INDEX.format(number=index_number)
-        reindex_service.source_client.indices.delete(index=index)
-        reindex_service.dest_client.indices.delete(index=index)
+        index = ES_TEST_INDEX.format(number=index_number)
+        elastic_client.source_client.indices.delete(index=index)
+        elastic_client.dest_client.indices.delete(index=index)
--- tests/integration/test_service.py
+++ tests/integration/test_service.py
@@ -3,50 +3,54 @@ from typing import List
 import pytest
 from elasticsearch import Elasticsearch
 
+from elasticsearch_reindex import Manager
+from elasticsearch_reindex.client import ElasticsearchClient
 from elasticsearch_reindex.errors import ElasticSearchNodeNotFoundException
-from elasticsearch_reindex.reindex import Index, ReindexService
+from elasticsearch_reindex.schema import Index
 from tests.conftest import TEST_INDEXES_COUNT
 
 INVALID_SOURCE_HOST = "localhost:9900"
 INVALID_DEST_HOST = "localhost:9901"
 
 
-def test_invalid_es_connection():
+def test_invalid_es_connection() -> None:
     """
     Test valid Elasticsearch client initialization.
     """
-    reindex_service = ReindexService(
+    es_client = ElasticsearchClient(
         source_es_host=INVALID_SOURCE_HOST, dest_es_host=INVALID_DEST_HOST
     )
     with pytest.raises(ElasticSearchNodeNotFoundException):
-        assert isinstance(reindex_service.source_client, Elasticsearch)
+        assert isinstance(es_client.source_client, Elasticsearch)
 
     with pytest.raises(ElasticSearchNodeNotFoundException):
-        assert isinstance(reindex_service.dest_client, Elasticsearch)
+        assert isinstance(es_client.dest_client, Elasticsearch)
 
 
-def test_valid_es_connection(reindex_service: ReindexService):
+def test_valid_es_connection(elastic_client: ElasticsearchClient) -> None:
     """
     Test valid Elasticsearch client initialization.
     """
-    assert isinstance(reindex_service.source_client, Elasticsearch)
+    assert isinstance(elastic_client.source_client, Elasticsearch)
+    assert isinstance(elastic_client.dest_client, Elasticsearch)
 
 
-def test_valid_indexes_creation(insert_indexes, reindex_service):
+def test_valid_indexes_creation(
+    insert_indexes, elastic_client: ElasticsearchClient
+) -> None:
     """
     Test valid indexes creation.
     """
-    source_client = reindex_service.source_client
-    data: List[Index] = reindex_service.get_all_indexes(client=source_client)
-
+    data: List[Index] = elastic_client.get_source_indexes()
     assert len(data) == TEST_INDEXES_COUNT
 
 
-def test_valid_data_migration(manager, reindex_service):
+def test_valid_data_migration(
+    manager: Manager, elastic_client: ElasticsearchClient
+) -> None:
     """
     Test successfully reindexing.
     """
-    dest_client = reindex_service.dest_client
-    indexes = reindex_service.get_all_indexes(client=dest_client)
-
+    manager.start_reindex()
+    indexes = elastic_client.get_dest_indexes()
     assert len(indexes) == TEST_INDEXES_COUNT
